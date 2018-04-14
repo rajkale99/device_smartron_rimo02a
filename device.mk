0b1b99243bb39b03bd0788adfd9f6c9a768f9997
Cuteprince <vishnudasvr07@gmail.com>
2018-04-14 07:36:33 +0000

    rimo02a: Build hidl hals
    Signed-off-by: Rk779 <rahulkrishna585@gmail.com>

--- device.mk
+++ device.mk
@@ -25,6 +25,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_AAPT_CONFIG := normal
 
+# HIDL HALS
+$(call inherit-product, $(LOCAL_PATH)/hidl-hals.mk)
+
 # Dalvik heap and hwui memory limits
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
